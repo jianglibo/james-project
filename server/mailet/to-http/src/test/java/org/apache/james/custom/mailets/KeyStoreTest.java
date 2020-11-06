e46990fe34a60cffe8be3b0663834787be1752a6
jianglibo <jianglibo@gmail.com>
2020-11-06 18:53:18 +0800

    support tls v.13 need jdk11

--- server/mailet/to-http/src/test/java/org/apache/james/custom/mailets/KeyStoreTest.java
+++ server/mailet/to-http/src/test/java/org/apache/james/custom/mailets/KeyStoreTest.java
@@ -6,10 +6,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.cert.CertificateException;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 
 import org.junit.jupiter.api.Test;
 
@@ -39,5 +46,40 @@ public class KeyStoreTest {
       System.out.println(urls[i].getFile());
     }
 
+  }
+
+  @Test
+  public void tls() {
+        SSLContext context = null;
+        try {
+            KeyStore keyStore = KeyStore.getInstance("pkcs12");
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("PKIX");
+            trustManagerFactory.init(keyStore);
+            TrustManager[] trustAllCerts = trustManagerFactory.getTrustManagers();
+            context = SSLContext.getInstance("TLSv1.3");
+            context.init(null, trustAllCerts, new SecureRandom());
+
+            SSLParameters params = context.getSupportedSSLParameters();
+            String[] protocols = params.getProtocols();
+            System.out.println("Java version : " + System.getProperty("java.runtime.version"));
+            boolean supportsTLSv13 = false;
+            for (String protocol : protocols) {
+                if ("TLSv1.3".equals(protocol)) {
+                    supportsTLSv13 = true;
+                    break;
+                }
+            }
+            if(supportsTLSv13) {
+                System.out.println("JRE supports TLS v1.3!");
+            } else {
+                System.out.println("JRE does NOT support TLS v1.3!");
+            }
+            String[] suites = params.getCipherSuites();
+            System.out.println("A total of " + suites.length + " TLS cipher suites is supported.");
+
+        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+            e.printStackTrace();
+            System.exit(42);
+        }
   }
 }
